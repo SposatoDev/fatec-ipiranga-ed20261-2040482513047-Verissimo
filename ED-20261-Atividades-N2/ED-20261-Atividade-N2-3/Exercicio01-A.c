504d76800b94dbd2f119fc4274dfae5beed8e175
Kauã Sato Sposato <131924519+SposatoDev@users.noreply.github.com>
2026-05-19 18:57:36 -0300

    Implement integer swap function in Exercicio01-A.c
    Added a function to swap two integers and updated main to demonstrate its usage.

--- ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio01-A.c
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio01-A.c
@@ -1,1 +1,30 @@
-printf("Hello, World!")
+// RA: 2040482513047 - Kauã Sato Sposato
+
+#include <stdio.h>
+
+void trocar(int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    printf("Antes da troca:\n");
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("Endereco de x: %p\n", (void*) &x);
+    printf("Endereco de y: %p\n", (void*) &y);
+
+    trocar(&x, &y);
+
+    printf("\nDepois da troca:\n");
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("Endereco de x: %p\n", (void*) &x);
+    printf("Endereco de y: %p\n", (void*) &y);
+
+    return 0;
+}
